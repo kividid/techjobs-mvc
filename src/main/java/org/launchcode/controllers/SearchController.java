09a8ed8dc8460859672b777e053befe082b6eeb4
Sam Thompson <sam@kividid.com>
2019-01-05 15:14:56 -0800

    added search results router and views for results, as well as views for list results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -3,8 +3,7 @@ package org.launchcode.controllers;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,12 +15,27 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @GetMapping(value = "results")
+    public String searchSubmit(Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
+        ArrayList<HashMap<String, String>> jobs;
 
+        if (searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        String title = "Searching: " + searchType + " for: " + searchTerm;
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("title", title);
+
+        return "search";
+    }
 }
